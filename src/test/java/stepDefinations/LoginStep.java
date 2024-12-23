0aaaf516dd68a5bb16ee912340f81c070d4d1ac5
Hameed <Hameeds270@gmail.com>
2024-12-23 16:04:19 +0530

    this is my second commit

--- src/test/java/stepDefinations/LoginStep.java
+++ src/test/java/stepDefinations/LoginStep.java
@@ -9,7 +9,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-
+//hi
 public class LoginStep {
 	private static String acTitle;
 	private static String title ;
---
+++ src/test/java/stepDefinations/jsut_for_github.java
@@ -0,0 +1,10 @@
+package stepDefinations;
+
+public class jsut_for_github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
